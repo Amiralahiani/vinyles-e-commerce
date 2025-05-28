7381fde0c6281dbb23be793c71d67de2510404ba
Malek <malek.azri@insat.ucar.tn>
2025-05-28 16:14:03 +0100

    localChanges

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -17,11 +17,12 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
-
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 final class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'admin_dashboard')]
+#[Route('/admin', name: 'admin_dashboard')]
 #[IsGranted('ROLE_ADMIN')]
 public function index(
     ProduitRepository $produitRepository,
